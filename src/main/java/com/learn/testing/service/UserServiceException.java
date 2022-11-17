1f67ba8918f616f6bb6631070f22f3c930918c43
patel571 <patel571@avaya.com>
2022-11-17 09:45:46 +0530

    Revert "Commit to add exception class"
    This reverts commit f7a4e929b281d6108c3993dc1dcfa295b0bb66c7.

--- src/main/java/com/learn/testing/service/UserServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.learn.testing.service;
-
-public class UserServiceException extends RuntimeException {
-    public UserServiceException(String message) {
-        super(message);
-    }
-}
